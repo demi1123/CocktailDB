33788cb2c6434038d191a20689ce96fa7192ed04
demi1123 <imdongning@gmail.com>
2021-03-30 14:07:40 +0800

    first commit cocktail db

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Esse adipisicing esse tempor officia laborum.Adipisicing 
+                laborum consequat velit reprehenderit in ipsum consectetur magna ex 
+                occaecat Lorem adipisicing. Voluptate cillum elit minim ex voluptate 
+                velit veniam. Incididunt ipsum laboris ut reprehenderit sunt eu ea aliquip 
+                Lorem. Cupidatat pariatur ad pariatur minim ex nostrud duis nostrud velit 
+                sunt sint sint deserunt excepteur. Excepteur anim et excepteur magna minim 
+                nisi consequat irure magna pariatur veniam. Excepteur in officia id sunt 
+                est quis enim nostrud anim ex velit consectetur laborum irure.
+            </p>
+        </section>
+    )
+}
+
+export default About
